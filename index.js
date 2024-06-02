d4762232b069ed789494ba03f13e921c83f4223c
Uday Bhatiya <udaybhatiya31@gmail.com>
2024-06-02 18:12:46 +0530

    animation with gsap and scroll trigger

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const projectSectionImageChange = () => {
     let elems = document.querySelectorAll('#projectContainer .project')
-const image = document.querySelector('#projectSection .container .img img')
+    const image = document.querySelector('#projectSection .container .img img')
 
-elems.forEach(function(e){
-    e.addEventListener('mouseenter', function(){
-        let img = e.getAttribute('data-img')
-        image.src = img
+    elems.forEach(function (e) {
+        e.addEventListener('mouseenter', function () {
+            let img = e.getAttribute('data-img')
+            image.src = img
+        })
     })
-})
 }
 
 const swipperJs = () => {
@@ -52,6 +52,204 @@ const popUpForm = () => {
     });
 }
 
+const heroSectionAnimation = () => {
+    gsap.from(' #heroSection .leftSection h3', {
+        opacity: 0,
+        delay: 0.5,
+        duration: 1
+    })
+    gsap.from(' #heroSection .leftSection h2', {
+        opacity: 0,
+        delay: 0.6,
+        duration: 1
+    })
+    gsap.from(' #heroSection .leftSection p', {
+        opacity: 0,
+        delay: 0.7,
+        duration: 1
+    })
+    gsap.from(' #heroSection .leftSection button', {
+        opacity: 0,
+        delay: 0.8,
+        duration: 1
+    })
+    gsap.from(' #heroSection .rightSection', {
+        opacity: 0,
+        delay: 0.9,
+        duration: 1
+    })
+}
+
+const serviceSectionAnimation = () => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '#serviceSection',
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+            // markers: true
+        }
+    });
+    
+    tl.from("#serviceSection .serviceHead .leftSection h2", {
+        opacity: 0,
+        y: 20,
+        duration: .8
+    })
+        .from("#serviceSection .serviceHead .leftSection h3", {
+            opacity: 0,
+            y: 20,
+            duration: .8
+        }, "-=0.5")
+        .from("#serviceSection .serviceHead .rightSection", {
+            opacity: 0,
+            y: 20,
+            duration: .8
+        }, "-=0.4");
+    
+    const slides = document.querySelectorAll('.swiper .swiper-wrapper .swiper-slide');
+    
+    slides.forEach(function (slide) {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#serviceSection',
+                start: 'top 50%',
+                toggleActions: 'play none none none',
+                // markers: true
+            }
+        });
+        tl.from(slide, {
+            opacity: 0,
+            y: 20,
+            duration: .8
+        })
+    })
+
+}
+
+const featuredSectionAnimation = () => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '#featuredSection',
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+            // markers: true
+        }
+    });
+    
+    tl.from("#featuredSection .heading h2", {
+        opacity: 0,
+        y: 20,
+        duration: .8
+    })
+        .from("#featuredSection .heading h3", {
+            opacity: 0,
+            y: 20,
+            duration: .8
+        }, "-=0.5")
+        
+        const cards = document.querySelectorAll('#featuredSection .cardContainer .card');
+        
+        cards.forEach(function (card) {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '#featuredSection',
+                    start: 'top 70%',
+                    toggleActions: 'play none none none',
+                    // markers: true
+                }
+            });
+            tl.from(card, {
+                opacity: 0,
+                y: 30,
+                duration: 2
+            })
+        })
+}
+
+const projectSectionAnimation = () => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '#projectSection',
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+            // markers: true
+        }
+    });
+    
+    tl.from("#projectSection .heading h2", {
+        opacity: 0,
+        y: 20,
+        duration: .8
+    })
+        .from("#projectSection .heading h3", {
+            opacity: 0,
+            y: 20,
+            duration: .8
+        }, "-=0.5")
+
+        .from("#projectSection .container", {
+            opacity: 0,
+            y: 20,
+            duration: .8
+        })
+}
+
+const companySectionAnimation = () => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '#companySection',
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+            // markers: true
+        }
+    });
+    
+    tl.from("#companySection .heading h2", {
+        opacity: 0,
+        y: 20,
+        duration: .8
+    })
+        .from("#companySection .heading h3", {
+            opacity: 0,
+            y: 20,
+            duration: .8
+        }, "-=0.5")  
+}
+
+const feedbackSectionAnimation = () => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '#feedbackSection',
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+            // markers: true
+        }
+    });
+    
+    tl.from("#feedbackSection .heading h2", {
+        opacity: 0,
+        y: 20,
+        duration: .8
+    })
+        .from("#feedbackSection .heading h3", {
+            opacity: 0,
+            y: 20,
+            duration: .8
+        }, "-=0.5")
+
+        .from("#feedbackSection .container", {
+            opacity: 0,
+            y: 20,
+            duration: .8
+        })
+}
+
 projectSectionImageChange()
 swipperJs()
 popUpForm()
+heroSectionAnimation()
+serviceSectionAnimation()
+featuredSectionAnimation()
+projectSectionAnimation()
+companySectionAnimation()
+feedbackSectionAnimation()
